e42814d2ecef10f690e69d4b01b4e9e0294a3a99
ixa532 <belobelle532@gmail.com>
2024-10-26 14:45:03 -0300

    Criação do método Update

--- Final_TowerDefence/Assets/Scripts/SpawnEnemy.cs
+++ Final_TowerDefence/Assets/Scripts/SpawnEnemy.cs
@@ -17,4 +17,17 @@ public class SpawnEnemy : MonoBehaviour
     private float timeLastSpawn;
     private int enemiesAlive;
     private int enemiesLeftSpawn;
+    private bool isSpawning = false;
+
+    private void Update()
+    {
+        timeLastSpawn += Time.deltaTime; //Acumulao o tempo desde o último spawn, aumentando a variavél com o tempo passado desde o último frame
+
+        if (timeLastSpawn >= (1f / enemiesPerSecond)) //Verifica se o tempo desde o último spawn é maior ou igual ao intervalo necessário para o próximo spawn
+        {
+            Debug.Log("Spawn Enemy"); //Se o tempo necessário passou, exibe uma mensagem no console indicando que um inimigo deve ser spawnado 
+        }
+    }
+
+  
 }

b844bb5ab24da85e600ea375824607dc6e1e87c2
ixa532 <belobelle532@gmail.com>
2024-10-27 15:46:30 -0300

    Lista de inimigos ativa

--- Final_TowerDefence/Assets/Scripts/LevelManager.cs
+++ Final_TowerDefence/Assets/Scripts/LevelManager.cs
@@ -8,7 +8,7 @@ public class LevelManager : MonoBehaviour
 
     public Transform startPoint;//Transform que define o ponto inicial do nível, usado para spawnar o ínicio do caminho  
     public Transform[] caminho; // Array de Transform que define os pontos de um caminho a ser seguido 
-
+    public List<Transform> inimigos = new List<Transform>(); //Lista de inimigos ativa 
     private void Awake() //Método Awake, chamado qaundo o objeto é criado, antes do Start
     {
         instance = this; // Define esta instância de LevelManager como a única instância acessível

35017e005a3073aa5935e40ba06f21c087fd0006
ixa532 <belobelle532@gmail.com>
2024-10-27 15:14:14 -0300

    Criação do Método Update

--- Final_TowerDefence/Assets/Tower.cs
+++ Final_TowerDefence/Assets/Tower.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Tower : MonoBehaviour
 {
-    [Header("References")]
     [SerializeField] private Transform towerRotationPoint;
     [SerializeField] private GameObject alcanceVisualizacao;
 
-    [Header("Attributes")]
     [SerializeField] private float targetRange = 5f;
-
     float escala;
+
+    private Transform alvo;
+
+    private void Update()
+    {
+        if (alvo == null)
+        {
+            FindAlvo();
+        }
+        else
+        {
+            RotateAlvo();
+        }
+    }
     void AjustarAlcance()
     {
         if (alcanceVisualizacao != null) //Verifica se o objeto visual do alcance existe na cena

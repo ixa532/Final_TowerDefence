b1c4d3100ec068e9c5347698e0870505cf7c1815
ixa532 <belobelle532@gmail.com>
2024-10-27 19:59:16 -0300

    Override método Shoot

--- Final_TowerDefence/Assets/IceTower.cs
+++ Final_TowerDefence/Assets/IceTower.cs
@@ -7,5 +7,11 @@ public class IceTower : Tower
     [SerializeField] private float slowAmount = 0.5f; //Percentual de redução da velocidade
     [SerializeField] private float slowDuration = 2f;//Duração do efeito de congelamento
 
-
+    protected override void Shoot()
+    {
+        GameObject bulletOBJ = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);//Instancia a bala no ponto de disparo
+        Bullet bulletScript = bulletOBJ.GetComponent<Bullet>();//Obtém o componente Bullet
+        bulletScript.SetAlvo(alvo); //Define o alvo para a bala
+        bulletScript.ApplySlowEffect(slowAmount, slowDuration); //Aplica o efeito de congelamento
+    }
 }

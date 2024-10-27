3fce639d2458e6de9042ff15504f4c0b92b6d5a1
ixa532 <belobelle532@gmail.com>
2024-10-27 15:48:25 -0300

    Criação do Método AddEnemy

--- Final_TowerDefence/Assets/Scripts/LevelManager.cs
+++ Final_TowerDefence/Assets/Scripts/LevelManager.cs
@@ -9,8 +9,15 @@ public class LevelManager : MonoBehaviour
     public Transform startPoint;//Transform que define o ponto inicial do nível, usado para spawnar o ínicio do caminho  
     public Transform[] caminho; // Array de Transform que define os pontos de um caminho a ser seguido 
     public List<Transform> inimigos = new List<Transform>(); //Lista de inimigos ativa 
+    
     private void Awake() //Método Awake, chamado qaundo o objeto é criado, antes do Start
     {
         instance = this; // Define esta instância de LevelManager como a única instância acessível
     }
+
+    public void AddEnemy(Transform inimigo)
+    { 
+        inimigos.Add(inimigo);
+    }
+
 }

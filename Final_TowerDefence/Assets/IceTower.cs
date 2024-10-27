69035279bbba645f5bbcb32d9e5c0b1509f65bf6
ixa532 <belobelle532@gmail.com>
2024-10-27 19:54:09 -0300

    Criação das variaveis

---
+++ Final_TowerDefence/Assets/IceTower.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceTower : Tower
+{
+    [SerializeField] private float slowAmount = 0.5f; //Percentual de redução da velocidade
+    [SerializeField] private float slowDuration = 2f;//Duração do efeito de congelamento
+
+
+}

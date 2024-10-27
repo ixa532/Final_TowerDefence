9acc6cfa871396740aaf58ef7324c704d3923d10
ixa532 <belobelle532@gmail.com>
2024-10-27 17:28:20 -0300

    Criação do método SetAlvo

--- Final_TowerDefence/Assets/Bullet.cs
+++ Final_TowerDefence/Assets/Bullet.cs
@@ -9,6 +9,10 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float bulletSpeed = 5f;
     [SerializeField] private Rigidbody rb;
 
+    public void SetAlvo(Transform _alvo) //Método publico que aceita um transform como paramêtro
+    {
+        alvo = _alvo; //Atribui o Transform passado como argumento para a variavel alvo
+    }
     private void FixedUpdate()
     {
         Vector2 direction = alvo.position - transform.position;

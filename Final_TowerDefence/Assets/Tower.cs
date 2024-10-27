67f130f6b44ca7f142378f8a78e33ac20a610b81
ixa532 <belobelle532@gmail.com>
2024-10-27 15:42:24 -0300

    Update Tower.cs

--- Final_TowerDefence/Assets/Tower.cs
+++ Final_TowerDefence/Assets/Tower.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.GraphicsBuffer;
 
 public class Tower : MonoBehaviour
 {

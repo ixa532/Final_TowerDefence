9d2953414f81733ba6acf32176ae521c8210e2fe
ixa532 <belobelle532@gmail.com>
2024-10-28 14:09:12 -0300

    Comentarios adicionados ao método EnemiesPerWave

--- Final_TowerDefence/Assets/Scripts/SpawnEnemy.cs
+++ Final_TowerDefence/Assets/Scripts/SpawnEnemy.cs
@@ -71,7 +71,7 @@ public class SpawnEnemy : MonoBehaviour
     }
 
 
-    private int EnemiesPerWave()
+    private int EnemiesPerWave()//Calcula e retorna o numero de inimigos para a onda atual
     {
         //Calcula e retorna o número de inimigos para a onda atual
         //Multiplica o número base de inimigos pelo valor da onda atual elevado ao fator de dificuldade

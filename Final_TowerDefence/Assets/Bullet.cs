9fb05a6830aa6e9912ade8a6ebdaa0dfe4ce6042
ixa532 <belobelle532@gmail.com>
2024-10-29 18:07:04 -0300

    Expicando o por que do uso do Other no método

--- Final_TowerDefence/Assets/Bullet.cs
+++ Final_TowerDefence/Assets/Bullet.cs
@@ -34,6 +34,7 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)//Método chamado quando o objeto colide com outro objeto 2D.
     {
+        //Other foi usado para poder acessar o gameObject no qual o objeto atual colidiu
         other.gameObject.GetComponent<Healthy>().TakeDamage();//Obtém o componente "Healthy" do objeto no qual teve colisão e aplica o dano chamando o método TakeDamage
         Destroy(gameObject);//Destrói o objeto atual (como uma bala ou inimigo) ao colidir com qualquer outro objeto.
 

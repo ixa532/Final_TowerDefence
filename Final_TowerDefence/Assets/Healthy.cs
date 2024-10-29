048062d6900bd37a0bc02591a3c48129ae6b2e9a
ixa532 <belobelle532@gmail.com>
2024-10-29 11:32:48 -0300

    Criação das variaveis

---
+++ Final_TowerDefence/Assets/Healthy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Healthy : MonoBehaviour
+{
+    [SerializeField] private int hitPoints = 2; //Variável que define a quantidade inicial de pontos de vida do inimigo 
+
+}

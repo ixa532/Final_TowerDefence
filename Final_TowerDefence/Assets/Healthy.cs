64f5b952d176ec8bc788d194b8dfe8c63a9ba09e
ixa532 <belobelle532@gmail.com>
2024-10-29 13:28:50 -0300

    Criação do método TakeDamage

--- Final_TowerDefence/Assets/Healthy.cs
+++ Final_TowerDefence/Assets/Healthy.cs
@@ -1,11 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
-using UnityEngine.VFX;
 
 public class Healthy : MonoBehaviour
 {
     [SerializeField] private int hitPoints = 2; //Variável que define a quantidade inicial de pontos de vida do inimigo 
 
+    public void TakeDamage(int damage)//Aplica dano ao objeto, reduzindo os pontos de vida
+    {
+        hitPoints = damage;//Reduz os pontos de vida do inimigo com base no valor de damage
+
+        if (hitPoints <= 0)//Verifica se os pontos de vida do inimigo com base no valor de damage
+        {
+            SpawnEnemy.onEnemyDestroy.Invoke(); //Dispara um sinal para o jogo informando que o inimigo foi destruído, permitindo que outros sistemas reajam a essa destruição
+            Destroy(gameObject);//Destroi o objeto
+        }
+    }
 }

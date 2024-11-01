fe7373d5da4a2a74261ab9b1dab951bd48eadda5
ixa532 <belobelle532@gmail.com>
2024-11-01 12:35:52 -0300

    método Start comentado

--- Final_TowerDefence/Assets/FireTower.cs
+++ Final_TowerDefence/Assets/FireTower.cs
@@ -7,7 +7,7 @@ public class FireTower : Tower
     [SerializeField]private float dano = 10f; //Dano causado por cada tiro 
     [SerializeField] private float fireRateMultiplier = 1.5f;//Multiplicador para a taxa de tiro
 
-    private void Start()
+    private void Start()//configura a taxa de disparo da torre logo no início, garantindo que ela seja ajustada com base no multiplicador fireRateMultiplier
     {
         //Ajusta a taxa de disparo inicial multiplicado pela taxa de fogo 
         bulletPerSecond *= fireRateMultiplier;

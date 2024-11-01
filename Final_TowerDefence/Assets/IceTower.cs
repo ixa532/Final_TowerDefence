1508b643d49cdfae25dc6aeb9b03db691b47c294
ixa532 <belobelle532@gmail.com>
2024-11-01 12:27:07 -0300

    Comentários adicionados ao método

--- Final_TowerDefence/Assets/IceTower.cs
+++ Final_TowerDefence/Assets/IceTower.cs
@@ -7,7 +7,9 @@ public class IceTower : Tower
     [SerializeField] private float slowAmount = 0.5f; //Percentual de redução da velocidade
     [SerializeField] private float slowDuration = 2f;//Duração do efeito de congelamento
 
-    protected override void Shoot()
+    //Método publico sobrescrito que realiza o disparo da torre.
+    // Ele instancia um objeto de bala na posição de disparo, define o alvo da bala e aplica um efeito de congelamento.
+    public override void Shoot()
     {
         GameObject bulletOBJ = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);//Instancia a bala no ponto de disparo
         Bullet bulletScript = bulletOBJ.GetComponent<Bullet>();//Obtém o componente Bullet

346a4fb626ec97afb58e203d203a341eaaa1c4b7
ixa532 <belobelle532@gmail.com>
2024-11-12 13:44:35 -0300

    FireTower
    Alteração nas variaveis
    Alteração no Método sobrescrito Shoot

--- Final_TowerDefence/Assets/Scripts/FireTower.cs
+++ Final_TowerDefence/Assets/Scripts/FireTower.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class FireTower : Tower
 {
-    [SerializeField]private float dano = 10f; //Dano causado por cada tiro 
-    [SerializeField] private float fireRateMultiplier = 1.5f;//Multiplicador para a taxa de tiro
+    
+    [SerializeField] private float fireDamagePerSecond = 1.5f;
+    [SerializeField] private float fireDuration = 4f;
 
-    private void Start()//configura a taxa de disparo da torre logo no início, garantindo que ela seja ajustada com base no multiplicador fireRateMultiplier
+    public override void Atacar()
     {
-        //Ajusta a taxa de disparo inicial multiplicado pela taxa de fogo 
-        bulletPerSecond *= fireRateMultiplier;
-    }
+        if (alvo != null) // Verifica se há um alvo designado
+        {
+            Healthy enemyHealthy = alvo.GetComponent<Healthy>(); // Obtém o componente de saúde do inimigo
 
-    public override void Shoot()//é responsável por realizar o processo de disparo da torre, configurando corretamente a bala para atingir o alvo e aplicar dano.
+            if (enemyHealthy != null) // Verifica se o inimigo possui o componente de saúde
+            {
+                StartCoroutine(ApplyFireDamage(enemyHealthy)); // Inicia a aplicação do dano de queimadura
+            }
+        }
+    }
+    private IEnumerator ApplyFireDamage(Healthy enemyHealthy)
+    {
+        float elapsedTime = 0f; // Tempo decorrido desde o início do efeito de queimadura
+
+        while (elapsedTime < fireDuration) // Continua aplicando dano até o fim da duração da queimadura
+        {
+          //  enemyHealthy.TakeDamage(fireDamagePerSecond * Time.deltaTime); // Aplica dano ao inimigo a cada frame
+            elapsedTime += Time.deltaTime; // Atualiza o tempo decorrido
+            yield return null; // Espera até o próximo frame para continuar
+        }
+    }
+public override void Shoot()//é responsável por realizar o processo de disparo da torre, configurando corretamente a bala para atingir o alvo e aplicar dano.
     {
         //Instancia a bala no ponto de disparo com rotação padrão
         GameObject bulletOBJ = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
@@ -24,7 +42,6 @@ public class FireTower : Tower
         //Define o alvo da bala usando o método SetAlvo
         bulletScript.SetAlvo(alvo);
 
-        //Adiciona dano à bala utilizando o método SetDano
-        bulletScript.SetDano(dano);
+        Atacar() ;
     }
 }

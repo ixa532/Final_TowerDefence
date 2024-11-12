8e3b5e13d8af0e887980d0cb042f62b2c483ff70
ixa532 <belobelle532@gmail.com>
2024-11-12 13:42:21 -0300

    EnemyMovement
    método UpdateSpeed com parametro
    com o método atualizando a velocidade do inimigo

--- Final_TowerDefence/Assets/Scripts/EnemyMovement.cs
+++ Final_TowerDefence/Assets/Scripts/EnemyMovement.cs
@@ -14,11 +14,7 @@ public class EnemyMovement : MonoBehaviour
 
     private Transform alvo; //Alvo atual para o qual o objeto irá se mover
     private int caminhoIndex = 0;//Índice do ponto atual no caminho, que inicia como 0
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
+    private float baseSpeed;
     private void Awake()// método usado para obter e armazenar a referência ao componente Rigidbody2D do objeto
     {
         rb = GetComponent<Rigidbody2D>(); //Obtém o componente Rigidbody2D do objeto que o script está anexado e o armazena na variável 'rb'.
@@ -27,6 +23,7 @@ public class EnemyMovement : MonoBehaviour
 
     private void Start() //método chamado para que o script seja iniciado
     {
+        baseSpeed = moveSpeed;
         alvo = LevelManager.instance.caminho[caminhoIndex];//Define o alvo inicial como o primeiro ponto no caminho, que é obtido pelo LevelManager 
     }
     private void Update() //Método chamado uma vez por frame
@@ -37,6 +34,7 @@ public class EnemyMovement : MonoBehaviour
 
             if (caminhoIndex == LevelManager.instance.caminho.Length)//Se o índice alcançar o fim do caminho, o objeto se destrói
             {
+                SpawnEnemy.onEnemyDestroy.Invoke();
                 Destroy(gameObject);//Destroi o objeto ao final do caminho
                 return;//Interrompe a execução para evitar erros ao tentar acessar alvo que não existe
             }
@@ -51,5 +49,14 @@ public class EnemyMovement : MonoBehaviour
     {
         Vector2 direction = (alvo.position - transform.position).normalized; //Calcula a direção normalizada do objeto em direção ao alvo atual //normalized permite que a velocidade de moveSpeed seja aplicada de forma que mova o objeto na direção do alvo na velocidade, sem alterar a direção do movimento; e converte o vetor em um vetor unitário ou seja Comprimento 1 
         rb.velocity = direction * moveSpeed;//Define a velocidade do Rigidbody do objeto, movendo para a direção calculada
+    }
+
+    public void UpdateSpeed(float newSpeed)
+    {
+        moveSpeed = newSpeed; //Atualizaa velocidade do inimigo
+    }
+    public void ResetSpeed()
+    {
+        moveSpeed = baseSpeed;
     }
 }

af0a19e3503af4c7204d53fe294138b001b032d9
ixa532 <belobelle532@gmail.com>
2024-11-12 13:56:50 -0300

    Update LevelManager
    Agora com uma vaiavel inteira com nome de currency
    
    Criação do método IncrementarMoedas, com parametro inteiro

--- Final_TowerDefence/Assets/Scripts/LevelManager.cs
+++ Final_TowerDefence/Assets/Scripts/LevelManager.cs
@@ -9,12 +9,17 @@ public class LevelManager : MonoBehaviour
     public Transform startPoint;//Transform que define o ponto inicial do nível, usado para spawnar o ínicio do caminho  
     public Transform[] caminho; // Array de Transform que define os pontos de um caminho a ser seguido 
     public List<Transform> inimigos = new List<Transform>(); //Lista de inimigos ativa 
+    public int currency;
     
     private void Awake() //Método Awake, chamado qaundo o objeto é criado, antes do Start
     {
         instance = this; // Define esta instância de LevelManager como a única instância acessível
     }
 
+    public void IncrementarMoedas(int amount)
+    {
+        currency += amount;
+    }
 
     // Método que permite adicionar um inimigo à lista de inimigos.
     // Recebe um Transform representando o inimigo a ser adicionado.

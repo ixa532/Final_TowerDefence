1add748bddab4e811f05f9055fdb1dfd337a7f50
ixa532 <belobelle532@gmail.com>
2024-11-12 13:52:04 -0300

    Update Healthy
    criação de mias uma variavel serializada inteira
    e uma variavel privada booleana, com valor falso
    Atualização hitPoints que agora é -=

--- Final_TowerDefence/Assets/Scripts/Healthy.cs
+++ Final_TowerDefence/Assets/Scripts/Healthy.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 
 public class Healthy : MonoBehaviour
 {
+    [SerializeField] private int qtMoedas = 40;
+    private bool isDestroyed = false;
     [SerializeField] private int hitPoints = 2; //Variável que define a quantidade inicial de pontos de vida do inimigo 
 
     public void TakeDamage(int damage)//Aplica dano ao objeto, reduzindo os pontos de vida
     {
-        hitPoints = damage;//Reduz os pontos de vida do inimigo com base no valor de damage
+        hitPoints -= damage;//Reduz os pontos de vida do inimigo com base no valor de damage
 
-        if (hitPoints <= 0)//Verifica se os pontos de vida do inimigo com base no valor de damage
+        if (hitPoints <= 0 && isDestroyed)//Verifica se os pontos de vida do inimigo com base no valor de damage
         {
+            LevelManager.instance.IncrementarMoedas(qtMoedas);
+            isDestroyed = true;
             SpawnEnemy.onEnemyDestroy.Invoke(); //Dispara um sinal para o jogo informando que o inimigo foi destruído, permitindo que outros sistemas reajam a essa destruição
             Destroy(gameObject);//Destroi o objeto
         }

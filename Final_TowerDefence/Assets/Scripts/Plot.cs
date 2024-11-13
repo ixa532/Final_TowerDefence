6178161b6f3367c4c0b88c71e2e066aff55e72a8
ixa532 <belobelle532@gmail.com>
2024-11-13 10:36:32 -0300

    Criação do método Start

--- Final_TowerDefence/Assets/Scripts/Plot.cs
+++ Final_TowerDefence/Assets/Scripts/Plot.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plot : MonoBehaviour
+public class Plot : MonoBehaviour //Representa uma área onde torres podem ser construídas no jogo.
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private SpriteRenderer sr;    // Componente SpriteRenderer usado para mudar a aparência do plot.
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Color hoverColor;    // Cor que será aplicada quando o mouse passar sobre o plot.
+
+    private GameObject tower;    // Referência ao objeto da torre construída neste plot.
+
+    private Color startColor;    // Cor inicial do plot.
+    
+    private void Start()// Método chamado ao iniciar o jogo. Inicializa a cor inicial do plot.
     {
-        
+        startColor = sr.color;// Armazena a cor inicial do SpriteRenderer.
     }
 }

fb1ccdeb3c5b5bf4237780af76c53eaaf02b8bd4
ixa532 <belobelle532@gmail.com>
2024-11-13 10:58:58 -0300

    Update Building.cs

--- Final_TowerDefence/Assets/Scripts/Building.cs
+++ Final_TowerDefence/Assets/Scripts/Building.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Building : MonoBehaviour//classe que gerencia a seleçaão e configuração das torres 
 {
-    public static Building Instance; // instancia estatica da classe para permitir o aceso global a seleção das torres
+    public Building Instance; // instancia estatica da classe para permitir o aceso global a seleção das torres
     [SerializeField] private Tower[] towers; //Array de torres disponivel para seleção
 
     private int selectedTower = 0; //índice da torre selecionada 

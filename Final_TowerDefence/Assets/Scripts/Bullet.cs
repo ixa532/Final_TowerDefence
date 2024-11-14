c380fac1efb540e92a95a06535c801d00c7e0623
ixa532 <belobelle532@gmail.com>
2024-11-14 08:54:56 -0300

    Update Bullet Script

--- Final_TowerDefence/Assets/Scripts/Bullet.cs
+++ Final_TowerDefence/Assets/Scripts/Bullet.cs
@@ -11,16 +11,11 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;//Referencia ao componente Rigidbody da bala para aplicar física 
     [SerializeField] private int bulletDamage = 1;
 
-    private Transform dano; //valor de dano que a bala causa´ra ao atingir o alvo
+    private Transform target; 
 
-    public void SetDano(Transform _dano) //Método privado para definir o valor de dano, recebendo um valor como parametro 
+    public void SetTarget(Transform _target) //Método publico que aceita um transform como paramêtro
     {
-        dano = _dano;//Define o valor de dano com o valor recebido como parametro
-    }
-
-    public void SetAlvo(Transform _alvo) //Método publico que aceita um transform como paramêtro
-    {
-        alvo = _alvo; //Atribui o Transform passado como argumento para a variavel alvo
+        target = _target;//Atribui o Transform passado como argumento para a variavel alvo
     }
     private void FixedUpdate()//responsável pela movimentação de um objeto em direção a um alvo. 
     {
@@ -28,12 +23,12 @@ public class Bullet : MonoBehaviour
 
         // Calcula a direção em que a bala deve se mover, subtraindo a posição do objeto atual da posição do alvo.
 
-        Vector2 direction = (alvo.position - transform.position).normalized ;
+        Vector2 direction = (target.position - transform.position).normalized;
 
         //// Atualiza a posição do objeto atual, movendo para a direção do alvo com a velocidade da bala, multiplicada pelo tempo desde o último frame
         rb.velocity = direction * bulletSpeed;
     }
-
+    
     private void OnCollisionEnter2D(Collision2D other)//Método chamado quando o objeto colide com outro objeto 2D.
     {
         //Other foi usado para poder acessar o gameObject no qual o objeto atual colidiu
